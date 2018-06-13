2e89e9f6a025ae04610c00ec52ca7514119d977f
ThePuffin <ballutvincent@gmail.com>
2018-06-13 17:12:55 +0200

    ajout theme.js

---
+++ src/assets/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  palette: {
+    primary: {
+      light: "#7a7cff",
+      main: "#304ffe",
+      dark: "#0026ca",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff5bff",
+      main: "#d500f9",
+      dark: "#9e00c5",
+      contrastText: "#fff"
+    }
+  }
+};
+
+export default theme;
--- src/components/App.js
+++ src/components/App.js
@@ -10,18 +10,17 @@ import './app.css'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-
+    return <div>
         <ul>
           <Link to="/">Home</Link> / <Link to="/about">About</Link>
         </ul>
-        <h1>La route des vins</h1>
-        <p className="test">Ceci est un test abcdefghijklmnopqrstuvwxyz</p>
+        <h1>LA route des vins</h1>
+        <p className="test">Ceci est un test</p>
+        <p> abcdefghijklmnopqrstuvwxyz</p>
+        <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-      </div>
-    );
+      </div>;
   }
 }
 
